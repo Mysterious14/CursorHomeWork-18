ffafeddb8be5f287d544bebd4f70eb5fd64882f0
Юрій Ірза <irzaurij04@gmail.com>
2022-10-27 16:59:19 +0300

    updates

--- src/index.js
+++ src/index.js
@@ -5,11 +5,13 @@ import './index.css';
 import reportWebVitals from './reportWebVitals';
 import App from './App';
 import { BrowserRouter } from 'react-router-dom';
+import HomePage from './components/HomePage';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
       <App />
+      <HomePage />
     </BrowserRouter>
   </React.StrictMode>,
 );
